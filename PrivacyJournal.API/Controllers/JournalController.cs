9f14c784bef8926cabde1cb9244c4d90188aefa0
hop <avinashseelall@gmail.com>
2023-03-17 01:09:10 -0400

    Calling_it_a_night

--- PrivacyJournal.API/Controllers/JournalController.cs
+++ PrivacyJournal.API/Controllers/JournalController.cs
@@ -12,8 +12,9 @@ namespace PrivacyJournal.API.Controllers
             return Ok(JournalStore.Current.Journals);
         }        
         [HttpPost]
-        public ActionResult<Journal> MakeJournal()
+        public ActionResult<Journal> MakeJournal(object id)
         {
+            Console.Write(id);
             JournalStore.Current.Journals.Add(new Journal{Id = 10, Title = "Bruh",Date = DateTime.Now, Entry = "pass"});
             return Ok(JournalStore.Current.Journals);
         }
--- privacyjournalreact/src/home/home.js
+++ privacyjournalreact/src/home/home.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./home.css"
 function Home() {
+  const [data, setData] = useState({})
+  const datas = async() =>{
+    const options = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'}
+      };
+      
+      fetch('http://localhost:5224/api/profile/', options)
+        .then(response => response.json())
+        .then(response => setData(response))
+        .catch(err => console.error(err));
+  }
+  useEffect(() => {
+
+
+    datas()
+  
+
+  }, [])
+  
   return (
-    <div className='homePage'>Home</div>
+    <div className='homePage'>{JSON.stringify(data)}</div>
   )
 }
 
